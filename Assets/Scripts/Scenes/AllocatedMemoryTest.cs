3fc72af3b61269309c85035464f831961edca3db
sindharta <sindharta@aiming-inc.com>
2014-11-21 16:05:19 +0900

    Added a scene to test the allocated memory

---
+++ Assets/Scripts/Scenes/AllocatedMemoryTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+
+public class AllocatedMemoryTest : MonoBehaviour {
+
+    List<byte[]> m_list = new List<byte[]>();
+
+//---------------------------------------------------------------------------------------------------------------------
+
+
+    // Update is called once per frame
+    void Update () {
+        m_list.Add(new byte[1024 * 1024]);  
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+}
