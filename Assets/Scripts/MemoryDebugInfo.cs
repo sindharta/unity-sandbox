413db3be6dcf35b2b06ff0eae14e31349aaa3122
sindharta <sindharta@aiming-inc.com>
2014-11-21 17:15:41 +0900

    Simplify MemoryDebugInfo

--- Assets/Scripts/MemoryDebugInfo.cs
+++ Assets/Scripts/MemoryDebugInfo.cs
@@ -46,23 +46,37 @@ public class MemoryDebugInfo: MonoBehaviour {
 
 //---------------------------------------------------------------------------------------------------------------------
 
-    private float m_lastUpdateTime = float.MinValue;
+    float m_lastUpdateTime = float.MinValue;
 
-    private float m_lastCollectTime = 0;
-    private float m_lastcollectNum = 0;
-    private float m_delta = 0;
-    private float m_lastDeltaTime = 0;
-    private int m_allocRate = 0;
-    private int m_allocMem = 0;
-    private int m_collectAlloc = 0;
-    private int m_peakAlloc = 0;
+    //GC
+    float m_lastCollectTime = 0;
+    int[] m_lastCollectCount;
+    float m_collectionFrequency = 0;
+
+    float m_lastDeltaTime         = 0;
+    int m_allocRate               = 0;
+    int m_allocMem                = 0;
+    int m_allocMemAfterCollection = 0;
+    int m_peakAlloc               = 0;
+
+    //Profiler
+    int m_allocMeshMemory     = 0;
+    int m_allocTextureMemory  = 0;
+    int m_allocAudioMemory    = 0;
+    int m_allocAnimationMemory= 0;
+    int m_allocMaterialMemory = 0;
 
 //---------------------------------------------------------------------------------------------------------------------
 
 
     void Awake() {
         useGUILayout = false;
+        m_lastCollectCount = new int[System.GC.MaxGeneration];
+        for (int i=0; i < System.GC.MaxGeneration; ++i) {
+            m_lastCollectCount[i] = 0;
+        }
     }
+
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -71,39 +85,69 @@ public class MemoryDebugInfo: MonoBehaviour {
             return;
         }
 
-
-
         //We want to know the exact frame rate when the garbage collection happened, so need to do this every frame
-        int collCount = System.GC.CollectionCount(0);
+        bool updated = UpdateGCCollectionCount();
 
-        //[TODO-Sin:2014-11-19]  until this part
-        if (m_lastcollectNum != collCount) {
-
-            m_lastcollectNum = collCount;
-
-
-            m_delta = Time.realtimeSinceStartup - m_lastCollectTime;
-
+        if (updated) {
+            m_collectionFrequency = Time.realtimeSinceStartup - m_lastCollectTime;
             m_lastCollectTime = Time.realtimeSinceStartup;
-
             m_lastDeltaTime = Time.deltaTime;
-
-            m_collectAlloc = m_allocMem;
+            m_allocMemAfterCollection = m_allocMem;
         }
 
 
-        if (Time.realtimeSinceStartup - m_lastUpdateTime > m_updateFrequency) {
-            int prev_alloc_memory = m_allocMem;
+        if (Time.realtimeSinceStartup - m_lastUpdateTime <= m_updateFrequency) 
+            return;
 
-            m_allocMem = (int)System.GC.GetTotalMemory(false);
-            m_peakAlloc = Mathf.Max(m_allocMem, m_peakAlloc);
+        //Start updating info
+        int prev_alloc_memory = m_allocMem;
 
-            int diff = m_allocMem - prev_alloc_memory;
-            m_allocRate = diff;
+        m_allocMem = (int)System.GC.GetTotalMemory(false);
+        m_peakAlloc = Mathf.Max(m_allocMem, m_peakAlloc);
 
-            m_lastUpdateTime = Time.realtimeSinceStartup;
+        int diff = m_allocMem - prev_alloc_memory;
+        m_allocRate = diff;
+
+        m_lastUpdateTime = Time.realtimeSinceStartup;
+
+
+#if ENABLE_PROFILER
+        if (!m_useProfiler)
+            return;
+        m_allocTextureMemory = GetRuntimeMemorySize(typeof(Texture));
+        m_allocMeshMemory = GetRuntimeMemorySize(typeof(Mesh));
+        m_allocAudioMemory = GetRuntimeMemorySize(typeof(AudioClip));
+        m_allocAnimationMemory = GetRuntimeMemorySize(typeof(Animation));
+        m_allocMaterialMemory = GetRuntimeMemorySize(typeof(Material));
+#endif
+
+    }
+//---------------------------------------------------------------------------------------------------------------------
+
+    static int GetRuntimeMemorySize(System.Type type) {
+        int ret = 0;
+        Object[] objects = Resources.FindObjectsOfTypeAll(type);
+        for (int i=0; i < objects.Length; ++i) {
+            ret += Profiler.GetRuntimeMemorySize(objects[i]);
+        }
+        return ret;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    bool UpdateGCCollectionCount() {
+
+        bool updated = false;
+        // Display collection counts.
+        for (int i = 0; i < System.GC.MaxGeneration; ++i) {
+            int count = System.GC.CollectionCount(i);
+            if (count!= m_lastCollectCount[i]) {
+                updated = true;
+                m_lastCollectCount[i] = count;
+            }
         }
 
+        return updated;
     }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -112,39 +156,36 @@ public class MemoryDebugInfo: MonoBehaviour {
             return;
         }
         StringBuilder text = new StringBuilder();
-
-        text.Append("Currently allocated			");
-        text.Append((m_allocMem / 1000000F).ToString("0"));
-        text.Append("mb\n");
-
-        text.Append("Peak allocated				");
-        text.Append((m_peakAlloc / 1000000F).ToString("0"));
-        text.Append("mb (last	collect ");
-        text.Append((m_collectAlloc / 1000000F).ToString("0"));
-        text.Append(" mb)\n");
-
-
-        text.Append("Allocation rate				");
-        text.Append((m_allocRate / 1000000F).ToString("0.0"));
-        text.Append("mb\n");
-
-        text.Append("Collection frequency		");
-        text.Append(m_delta.ToString("0.00"));
-        text.Append("s\n");
-
-        text.Append("Last collect m_delta			");
-        text.Append(m_lastDeltaTime.ToString("0.000"));
-        text.Append("s (");
-        text.Append((1F / m_lastDeltaTime).ToString("0.0"));
-
-        text.Append(" fps)");
+        text.AppendStrings("Currently allocated			", ToMB(m_allocMem), "MB\n");
+        text.AppendStrings("Peak allocated:				", ToMB(m_peakAlloc), "MB ( Allocated ");
+        text.AppendStrings("after collection: ", ToMB(m_allocMemAfterCollection), " MB)\n");
+        text.AppendStrings("Allocation rate				", ToMB(m_allocRate), "MB\n");
+        text.AppendStrings("Collection frequency		", m_collectionFrequency.ToString("0.00"), "s\n");                           
+        text.AppendStrings("Delta time dur. collection  ", m_lastDeltaTime.ToString("0.000"));
+        text.AppendStrings("s (", (1F / m_lastDeltaTime).ToString("0.0"), " fps)\n");
 
         if (m_showFPS) {
-            text.Append("\n" + (1F / Time.deltaTime).ToString("0.0") + " fps");
+            text.AppendStrings((1F / Time.deltaTime).ToString("0.0") , " fps\n");
         }
 
-        GUI.Box(new Rect(5, 5, 310, 80 + (m_showFPS ? 16 : 0)), "");
+        //profiler
+        if (m_useProfiler) 
+        {
+            text.AppendStrings("Mesh:			", ToMB(m_allocMeshMemory), "MB\n");
+            text.AppendStrings("Texture			", ToMB(m_allocTextureMemory), "MB\n");
+            text.AppendStrings("AudioClip:		", ToMB(m_allocAudioMemory), "MB\n");
+            text.AppendStrings("Animation:		", ToMB(m_allocAnimationMemory), "MB\n");
+            text.AppendStrings("Material:		", ToMB(m_allocMaterialMemory), "MB\n");
+        }
+
+        GUI.Box(new Rect(5, 5, 500, 160 + (m_showFPS ? 16 : 0)), "");
         GUI.Label(new Rect(10, 5, 1000, 200), text.ToString());
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static string ToMB(int bytes) {
+        return (bytes / 1000000F).ToString("0");
     }
 
 //---------------------------------------------------------------------------------------------------------------------
