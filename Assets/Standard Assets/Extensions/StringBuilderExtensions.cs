c7b1cfb262afbba88e577976888346eb9fed9eeb
sindharta <sindharta@aiming-inc.com>
2014-11-21 17:37:31 +0900

    Added a StringBuilder extension to add multiple strings

---
+++ Assets/Standard Assets/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+public static class StringBuilderExtensions {
+
+    public static void AppendStrings(this StringBuilder builder, params string[] strings) {
+
+        for (int i=0; i < strings.Length; ++i) {
+            builder.Append(strings[i]);
+        }
+    }
+
+}
