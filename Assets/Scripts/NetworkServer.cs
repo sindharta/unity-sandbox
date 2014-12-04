c1024a1fbcec43fd5515a6179e3539d343fa44e0
sindharta <sindharta@aiming-inc.com>
2014-12-04 11:25:22 +0900

    Made a test to run a server and a client within one application

---
+++ Assets/Scripts/NetworkServer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+public class NetworkServer {
+
+    TcpListener m_tcpServer = null;
+
+    bool m_stopRequested = false;
+
+    public Action<TcpClient> ClientAccepted = null;
+
+
+//---------------------------------------------------------------------------------------------------------------------
+    public void StartServer() {
+        IPAddress local_addr = IPAddress.Parse("127.0.0.1");
+        m_tcpServer = new TcpListener(local_addr, Constants.NETWORK_PORT);
+        m_tcpServer.Start();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public void StartListen() {
+        while (!m_stopRequested) {
+            TcpClient client = m_tcpServer.AcceptTcpClient();
+            if (null!=ClientAccepted) {
+                ClientAccepted(client);
+            }
+
+        }
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public void RequestStop() {
+        m_tcpServer.Stop();
+        m_stopRequested = true;
+    }
+
+
+}
+
---
+++ Assets/Standard Assets/Extensions/TcpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+public static class TcpClientExtensions {
+
+
+    // Detect if client disconnected
+    //https://social.msdn.microsoft.com/Forums/en-US/c857cad5-2eb6-4b6c-b0b5-7f4ce320c5cd/c-how-to-determine-if-a-tcpclient-has-been-disconnected?forum=netfxnetcom
+    public static bool IsDisconnected(this TcpClient tcpClient) {
+        if (tcpClient.Client.Poll(0, SelectMode.SelectRead)) {
+            byte[] buff = new byte[1];
+            if (tcpClient.Client.Receive(buff, SocketFlags.Peek) == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}
