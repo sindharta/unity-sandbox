dcc815e20eab725baca81c6fe4505f2c2c3ce6bb
sindharta <sindharta@aiming-inc.com>
2014-12-17 10:42:38 +0900

    Added a test to send/receive data using UDP

---
+++ Assets/Scripts/Network/UdpBaseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+abstract class UdpBaseController : MonoBehaviour
+{
+    UdpReceiverThread m_receiverThread;
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    protected void InitReceiverThread(UdpClient udpClient, IPEndPoint ip, Action<byte[]> dataReceivedCallback)
+    {
+        m_receiverThread = new UdpReceiverThread(udpClient, ip);
+        m_receiverThread.DataReceived = dataReceivedCallback;
+
+        Thread thread = new Thread(new ThreadStart(m_receiverThread.StartReceive));
+        thread.Start();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void OnDestroy() {
+        m_receiverThread.RequestStop();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+
+}
---
+++ Assets/Scripts/Network/UdpReceiverThread.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System;
+using System.Net;
+
+public class UdpReceiverThread {
+    UdpClient m_client = null;
+    IPEndPoint m_sourceIp;
+    bool m_stopRequested = false;
+
+    public Action<byte[]> DataReceived = null;
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public UdpReceiverThread(UdpClient client, IPEndPoint sourceIp) {
+        m_client = client;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public void StartReceive() {
+        while (!m_stopRequested) {
+            byte[] data = m_client.Receive(ref m_sourceIp);
+            if (null != DataReceived) {
+                DataReceived(data);
+            }
+        }
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    public void RequestStop() {
+        m_client.Close();
+        m_stopRequested = true;
+    }
+}
