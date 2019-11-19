567f7f0bfb6bf01b702607c35087930a97db1868
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-11-19 16:58:57 +0900

    Adding a custom posteffect

---
+++ Assets/Scripts/PostProcessing/Grayscale.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing; //PostProcessEffectSettings
+
+[Serializable]
+[PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/Grayscale")]
+public sealed class Grayscale : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
+    public FloatParameter blend = new FloatParameter { value = 0.5f };
+}
---
+++ Assets/Scripts/PostProcessing/GrayscaleRenderer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public sealed class GrayscaleRenderer : PostProcessEffectRenderer<Grayscale> {
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Grayscale"));
+        sheet.properties.SetFloat("_Blend", settings.blend);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
