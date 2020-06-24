02f03e938d4ed3df5d215f57c6b56bac0dae0cf2
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-06-24 11:28:48 +0900

    Add DOTS script to spawn balls

---
+++ Assets/Scripts/DOTS/DOTSBallSpawnAuthoringComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct DOTSBallSpawnAuthoringComponent : IComponentData {
+    public Entity BallPrefab;
+    public int NumBalls;    
+}
+
---
+++ Assets/Scripts/DOTS/DOTSBallSpawnSystem.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class DOTSBallSpawnSystem : SystemBase {
+    private struct DOTSBallSpawned : IComponentData {
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    protected override void OnCreate() {
+        base.OnCreate();
+        m_beginInitializationECBS = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }    
+    
+//----------------------------------------------------------------------------------------------------------------------    
+
+    protected override void OnUpdate() {
+        EntityCommandBuffer cmd = m_beginInitializationECBS.CreateCommandBuffer();
+        
+        JobHandle jobHandle =  Entities
+            .WithNone<DOTSBallSpawned>()
+            .ForEach((Entity entity, ref DOTSBallSpawnAuthoringComponent spawnAC) => {
+                if (spawnAC.NumBalls <= 0) {
+                    cmd.AddComponent<DOTSBallSpawned>(entity);
+                    return;
+                }
+                
+                if (null == spawnAC.BallPrefab) {
+                    Debug.LogError("Prefab is not set in DOTSBallSpawnAuthoringComponent");
+                    return;
+                }
+        
+        
+                Entity instance = cmd.Instantiate(spawnAC.BallPrefab);
+                
+                cmd.AddComponent<Translation>(instance, new Translation() {
+                    Value = new float3(spawnAC.NumBalls * 0.1f,0,0)
+                });
+                spawnAC.NumBalls = spawnAC.NumBalls-1;
+        
+            }).Schedule(Dependency);
+        
+        
+        // Make sure that the ECB system knows about our job
+        m_beginInitializationECBS.AddJobHandleForProducer(jobHandle);
+        
+        
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    
+    BeginInitializationEntityCommandBufferSystem m_beginInitializationECBS;
+
+}
+
+    
