5e613eb0b96461c681122eb8a2c5c61df7cc35ac
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-06-24 11:43:19 +0900

    Fix null check

--- Assets/Scripts/DOTS/DOTSBallSpawnSystem.cs
+++ Assets/Scripts/DOTS/DOTSBallSpawnSystem.cs
@@ -28,19 +28,19 @@ public class DOTSBallSpawnSystem : SystemBase {
                     return;
                 }
                 
-                if (null == spawnAC.BallPrefab) {
+                if (Entity.Null == spawnAC.BallPrefab) {
+                    cmd.AddComponent<DOTSBallSpawned>(entity);
                     Debug.LogError("Prefab is not set in DOTSBallSpawnAuthoringComponent");
                     return;
                 }
-        
-        
+
                 Entity instance = cmd.Instantiate(spawnAC.BallPrefab);
                 
-                cmd.AddComponent<Translation>(instance, new Translation() {
+                cmd.SetComponent(instance, new Translation() {
                     Value = new float3(spawnAC.NumBalls * 0.1f,0,0)
                 });
                 spawnAC.NumBalls = spawnAC.NumBalls-1;
-        
+
             }).Schedule(Dependency);
         
         
