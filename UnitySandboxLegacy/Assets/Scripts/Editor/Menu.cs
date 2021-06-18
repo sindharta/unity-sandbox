76d0a9e73d1b20cb05d97f8afa532c731873bba0
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-06-18 12:21:05 +0900

    legacy: add GrayScene to test gray colors (linear/srgb) (#44)
    * Add Gray scene
    * Add materials in the scene
    * Add Textures
    * Add TrueGray shader
    * Add a simplied version of Unlit/Color shader
    * Add materials
    * Add camera output RTs
    * Add Debug menu script
    
    
    Others:
    * Update project to Unity 2019.4.28f1

---
+++ UnitySandboxLegacy/Assets/Scripts/Editor/Menu.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using Unity.Assertions;
+using Unity.Collections;
+
+public static class Menu {
+
+    [MenuItem("Debug/Create SRGB RenderTexture")]
+    static void CreateSRGBRenderTexture() {             
+        RenderTexture colorMap = new RenderTexture(256, 256, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB);
+        colorMap.Create();
+        string path = "Assets/SRGB_RenderTexture.rendertexture";
+        AssetDatabase.CreateAsset(colorMap, path);
+        Debug.Log("SRGB RenderTexture created in " + path);
+    }    
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    [MenuItem("Debug/Create Gray Texture")]
+    static void CreateGrayTexture() {
+
+        const int WIDTH   = 4;
+        const int HEIGHT  = 4;
+        Texture2D tex = new Texture2D(WIDTH, HEIGHT, TextureFormat.RGBA32 , mipChain: false, linear: true);
+        
+        NativeArray<Color32> texData = tex.GetRawTextureData<Color32>();
+
+        int index = 0;
+        for (int j = 0; j < HEIGHT; ++j) {
+            for (int i = 0; i < WIDTH; ++i) {
+                float grayscale = 0.5f;
+                Color col       = new Color(grayscale, grayscale, grayscale, 1);                
+                texData[index++] = col; 
+            }
+        }
+        
+        tex.Apply();
+        
+        byte[] encodedData = null;
+        encodedData = tex.EncodeToPNG();
+        string path = "Assets/Textures/Gray.png";
+        File.WriteAllBytes("Assets/Textures/Gray.png", encodedData);
+        Debug.Log("Gray texture created in " + path);
+        
+        //Cleanup
+        UnityEngine.Object.DestroyImmediate(tex);
+        AssetDatabase.Refresh();
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    
+    [MenuItem("Debug/Encode Gray Scene Output RT")]
+    static void EncodeGraySceneOutputRT() {
+
+        string root = "Assets/Scenes/GrayScene/";
+
+        string[] rtFileNames = new string[] {
+            "GraySceneOutputLinearRT",
+            "GraySceneOutputSrgbRT",
+
+        };
+        
+        foreach (string fileName in rtFileNames){
+            string rtFullPath = Path.Combine(root, fileName + ".renderTexture");
+            string outputPath = fileName + ".png";
+            CaptureToFile(rtFullPath, outputPath, TextureFormat.RGBA32);
+            Debug.Log("RT encoded to " + outputPath);
+        }
+    }
+    
+    static void CaptureToFile(string inputRenderTexturePath, string outputFilePath, TextureFormat textureFormat) {
+
+        RenderTexture rt = AssetDatabase.LoadAssetAtPath<RenderTexture>(inputRenderTexturePath);
+        Assert.IsNotNull(rt);
+        
+        RenderTexture prevRenderTexture = RenderTexture.active;
+        RenderTexture.active = rt;
+                   
+        Texture2D tempTex = new Texture2D(rt.width, rt.height, textureFormat,mipChain: false);
+        tempTex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0, false);
+        tempTex.Apply();
+
+        try {
+            byte[] encodedData = tempTex.EncodeToPNG();             
+            File.WriteAllBytes(outputFilePath, encodedData);
+        } catch (Exception e) {
+            Debug.LogError($"Can't write to file: {outputFilePath}." + Environment.NewLine 
+                + $"Error: {e.ToString()}"); 
+        }
+       
+        //Cleanup
+        UnityEngine.Object.DestroyImmediate(tempTex);
+        RenderTexture.active = prevRenderTexture;        
+    }
+
+
+}
