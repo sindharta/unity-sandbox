3c691fc078da5c4bbb5d2f0eeeab88d415aa7b76
sindharta <sindharta@gmail.com>
2021-06-18 19:30:27 +0900

    refactor

--- UnitySandboxMLAPI/Assets/Scripts/NetworkVariableTest.cs
+++ UnitySandboxMLAPI/Assets/Scripts/NetworkVariableTest.cs
@@ -2,51 +2,46 @@ using MLAPI;
 using MLAPI.NetworkVariable;
 using UnityEngine;
 
-public class NetworkVariableTest : NetworkBehaviour
-{
-    private NetworkVariable<float> ServerNetworkVariable = new NetworkVariable<float>();
-    private NetworkVariable<float> ClientNetworkVariable = new NetworkVariable<float>();
-    private float                  last_t                = 0.0f;
+public class NetworkVariableTest : NetworkBehaviour {
 
-    void Start()
-    {
+    void Start() {
         ClientNetworkVariable.Settings.WritePermission = NetworkVariablePermission.OwnerOnly;
         ClientNetworkVariable.Settings.ReadPermission  = NetworkVariablePermission.ServerOnly;
 
-        if (IsServer)
-        {
+        if (IsServer) {
             ServerNetworkVariable.Value = 0.0f;
             Debug.Log("Server's var initialized to: " + ServerNetworkVariable.Value);
         }
-        else if (IsClient)
-        {
+        else if (IsClient) {
             ClientNetworkVariable.Value = 0.0f;
             Debug.Log("Client's var initialized to: " + ClientNetworkVariable.Value);
         }
     }
 
-    void Update()
-    {
-        var t_now = Time.time;
-        if (IsServer)
-        {
+    void Update() {
+        float timeNow = Time.time;
+        if (IsServer) {
             ServerNetworkVariable.Value = ServerNetworkVariable.Value + 0.1f;
-            if (t_now - last_t > 0.5f)
-            {
-                last_t = t_now;
-                Debug.Log("Server set its var to: " + ServerNetworkVariable.Value + ", has client var at: "  + 
+            if (timeNow - m_lastLogTime > 0.5f) {
+                m_lastLogTime = timeNow;
+                Debug.Log("Server set its var to: " + ServerNetworkVariable.Value + ", has client var at: " +
                     ClientNetworkVariable.Value);
             }
         }
-        else if (IsClient)
-        {
+        else if (IsClient) {
             ClientNetworkVariable.Value = ClientNetworkVariable.Value + 0.1f;
-            if (t_now - last_t > 0.5f)
-            {
-                last_t = t_now;
-                Debug.Log("Client set its var to: " + ClientNetworkVariable.Value + ", has server var at: "  + 
+            if (timeNow - m_lastLogTime > 0.5f) {
+                m_lastLogTime = timeNow;
+                Debug.Log("Client set its var to: " + ClientNetworkVariable.Value + ", has server var at: " +
                     ServerNetworkVariable.Value);
             }
         }
     }
+
+//----------------------------------------------------------------------------------------------------------------------
+    
+    private NetworkVariable<float> ServerNetworkVariable = new NetworkVariable<float>();
+    private NetworkVariable<float> ClientNetworkVariable = new NetworkVariable<float>();
+    private float                  m_lastLogTime       = 0.0f;
+    
 }
