8603713fd2091b21567a17d49bfcd7f9519c7717
sindharta <sindharta@gmail.com>
2021-06-18 19:42:06 +0900

    minor

--- UnitySandboxMLAPI/Assets/Scripts/NetworkTransformTest.cs
+++ UnitySandboxMLAPI/Assets/Scripts/NetworkTransformTest.cs
@@ -2,12 +2,9 @@ using System;
 using MLAPI;
 using UnityEngine;
 
-public class NetworkTransformTest : NetworkBehaviour
-{
-    void Update()
-    {
-        if (IsClient)
-        {
+public class NetworkTransformTest : NetworkBehaviour {
+    void Update() {
+        if (IsClient) {
             float theta = Time.frameCount / 10.0f;
             transform.position = new Vector3((float) Math.Cos(theta), 0.0f, (float) Math.Sin(theta));
         }
