5884f9cf2889bb9e226a6d08981e6b082a66bebd
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-09-07 20:35:55 +0900

    move players automatically

--- UnitySandboxNetcode/Assets/Scripts/HelloWorldManager.cs
+++ UnitySandboxNetcode/Assets/Scripts/HelloWorldManager.cs
@@ -1,17 +1,29 @@
+using System;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 namespace HelloWorld {
 public class HelloWorldManager : MonoBehaviour {
     void OnGUI() {
-        GUILayout.BeginArea(new Rect(10, 10, 300, 600));
-        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        bool isServer = NetworkManager.Singleton.IsServer;
+        if (!NetworkManager.Singleton.IsClient && !isServer) {
             DrawStartButtons();
         } else {
             DrawStatusLabels();
             DrawSubmitNewPositionButton();
         }
 
+        GUILayout.EndArea();
+        
+        if (!isServer) 
+            return;
+
+        GUILayout.BeginArea(new Rect(10, 310, 300, 300));
+        if (GUILayout.Button(m_autoMove ? "Turn OFF auto-move" : "Turn ON auto-move")) {
+            m_autoMove = !m_autoMove;
+        }
         GUILayout.EndArea();
     }
 
@@ -40,5 +52,29 @@ public class HelloWorldManager : MonoBehaviour {
             }
         }
     }
+
+    void Update() {
+        bool isServer = NetworkManager.Singleton.IsServer;
+        if (!isServer)
+            return;
+
+        if (!m_autoMove)
+            return;
+        
+        //Move players automatically
+        foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds) { 
+            HelloWorldPlayer   player = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<HelloWorldPlayer>();
+            
+            float   theta = Time.frameCount / 10.0f;
+            float   dist  =  (uid + 1) * 1.5f;
+            Vector3 pos   = new Vector3((float)Math.Cos(theta) * dist, 0.0f, (float)Math.Sin(theta) * dist);
+            player.NetworkMoveByServer(pos);
+        }
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    private bool m_autoMove = false;
+
 }
 }
