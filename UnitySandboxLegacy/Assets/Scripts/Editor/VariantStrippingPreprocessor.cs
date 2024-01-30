c2da25f824ee2756e8a01441559232956c8ae610
sin <sindharta.tanuwijaya@unity3d.com>
2024-01-30 17:06:55 +0900

    output debug log

--- UnitySandboxLegacy/Assets/Scripts/Editor/VariantStrippingPreprocessor.cs
+++ UnitySandboxLegacy/Assets/Scripts/Editor/VariantStrippingPreprocessor.cs
@@ -6,10 +6,12 @@ using UnityEditor.Build;
 using UnityEditor.Rendering;
 
 class VariantStrippingPreprocessor : IPreprocessShaders  {
-    // Use callbackOrder to set when Unity calls this shader preprocessor. Unity starts with the preprocessor that has the lowest callbackOrder value.
     public int callbackOrder { get { return 0; } }
 
     public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data) {
+        
+        Debug.Log($"Shader {shader.name}, Type: {snippet.shaderType}, numVariants: {data.Count}");
+        
         for (int i = 0; i < data.Count; ++i) {
             if (!data[i].shaderKeywordSet.IsEnabled(m_KeywordToStrip) || EditorUserBuildSettings.development) 
                 continue;
