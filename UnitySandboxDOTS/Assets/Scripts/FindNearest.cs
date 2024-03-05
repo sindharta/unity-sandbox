bcae00ea1faa617f344baa87acbfc4b51c4bc2c1
sin <sindharta.tanuwijaya@unity3d.com>
2024-03-05 20:47:00 +0900

    Add FindNearest

---
+++ UnitySandboxDOTS/Assets/Scripts/FindNearest.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FindNearest : MonoBehaviour
+{
+    public void Update()
+    {
+        // Find the nearest Target.
+        // When comparing distances, it's cheaper to compare
+        // the squares of the distances because doing so
+        // avoids computing square roots.
+        int seekerCount = Spawner.SeekerTransforms.Length;
+        int targetCount = Spawner.TargetTransforms.Length;
+        for (int i = 0; i < seekerCount; ++i) {
+            Transform seekerTransform = Spawner.SeekerTransforms[i];
+            Vector3 seekerPos = seekerTransform.localPosition;
+            Vector3 nearestTargetPos = default;
+            float nearestDistSq = float.MaxValue;
+
+            
+            for (int j = 0; j < targetCount; ++j) {
+                Transform targetTransform = Spawner.TargetTransforms[j];
+                Vector3 offset = targetTransform.localPosition - seekerPos;
+                float distSq = offset.sqrMagnitude;
+
+                if (distSq < nearestDistSq)
+                {
+                    nearestDistSq = distSq;
+                    nearestTargetPos = targetTransform.localPosition;
+                }
+            }
+ 
+            Debug.DrawLine(seekerPos, nearestTargetPos);
+        }        
+    }
+}
