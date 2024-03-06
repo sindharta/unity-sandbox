fea5148270e1ecba72fae36980b36e105cc9e6de
sin <sindharta.tanuwijaya@unity3d.com>
2024-03-06 20:37:57 +0900

    minor

--- UnitySandboxDOTS/Assets/Components/TankAuthoring.cs
+++ UnitySandboxDOTS/Assets/Components/TankAuthoring.cs
@@ -6,14 +6,11 @@ public class TankAuthoring : MonoBehaviour
     public GameObject Turret;
     public GameObject Cannon;
     
-    class Baker : Baker<TankAuthoring>
-    {
-        public override void Bake(TankAuthoring authoring)
-        {
+    class Baker : Baker<TankAuthoring> {
+        public override void Bake(TankAuthoring authoring) {
             // GetEntity returns the Entity baked from the GameObject
-            var entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
-            AddComponent(entity, new Tank
-            {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Tank {
                 Turret = GetEntity(authoring.Turret, TransformUsageFlags.Dynamic),
                 Cannon = GetEntity(authoring.Cannon, TransformUsageFlags.Dynamic)
             });
@@ -22,8 +19,7 @@ public class TankAuthoring : MonoBehaviour
 }
 
 // A component that will be added to the root entity of every tank.
-public struct Tank : IComponentData
-{
+public struct Tank : IComponentData {
     public Entity Turret;
     public Entity Cannon;
 }
