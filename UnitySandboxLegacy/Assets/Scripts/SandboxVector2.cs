a4ca0b12773fd9cea80f7221e8541d48ae91b48e
sin <sindharta.tanuwijaya@unity3d.com>
2024-05-14 22:48:26 +0900

    simplifying

--- UnitySandboxLegacy/Assets/Scripts/SandboxVector2.cs
+++ UnitySandboxLegacy/Assets/Scripts/SandboxVector2.cs
@@ -1,31 +1,14 @@
+using System;
 using System.Diagnostics;
+using UnityEngine;
 
-[DebuggerTypeProxy(typeof(SandboxVector2DebugView))]
 [DebuggerDisplay("({X},{Y})")]
 public struct SandboxVector2 {
-
-    internal class SandboxVector2DebugView {
-        public SandboxVector2DebugView(SandboxVector2 myVec)
-        {
-            this.m_myVec = myVec;
-        }
-
-        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-        public float X => m_myVec.X;
-
-        private SandboxVector2 m_myVec;
-        
-        public const string TestString = "This should appear in the debug window.";
-    }    
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------    
     
     public SandboxVector2(float x, float y) {
         this.X = x;
         this.Y = y;
     }
-    
-    private const string TestString = "This should not appear in the debug window.";    
-
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------    
     
     public float X;
