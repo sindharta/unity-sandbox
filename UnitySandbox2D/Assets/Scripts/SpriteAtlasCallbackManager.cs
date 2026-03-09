cca1413faa53c129e91789eafed6d8b73ef1cb21
sin <sindharta@gmail.com>
2026-03-09 17:02:45 +0900

    SpriteAtlasCallbackManager
    (cherry picked from commit 33e3d8fdf69768c15c599a5a4e29547b22b938c2)

---
+++ UnitySandbox2D/Assets/Scripts/SpriteAtlasCallbackManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class SpriteAtlasCallbackManager : MonoBehaviour {
+
+    private void OnEnable() {
+        SpriteAtlasManager.atlasRequested += OnAtlasRequested;
+        
+    }
+    
+    private void OnDisable() {
+        SpriteAtlasManager.atlasRequested -= OnAtlasRequested;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    private void OnAtlasRequested(string atlasName, System.Action<SpriteAtlas> callback)
+    {
+        Debug.Log($"Atlas requested: {atlasName}");
+        // Load SpriteAtlas from Addressables
+        var handle = Addressables.LoadAssetAsync<SpriteAtlas>("Assets/Art/AtlasAssetBundles/UnityChanExtraActionsAtlas.spriteatlasv2");
+        handle.Completed += (AsyncOperationHandle<SpriteAtlas> op) =>
+        {
+            if (op.Status == AsyncOperationStatus.Succeeded && op.Result != null)
+            {
+                callback(op.Result);
+            }
+            else
+            {
+                Debug.LogError($"Failed to load SpriteAtlas '{atlasName}' from Addressables.");
+                callback(null);
+            }
+        };
+    }
+
+
+}
